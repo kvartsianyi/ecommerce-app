9b87fd58462ad97efc6bcbd367e97ffd4ffd6c72
Yuriy <liparg104@gmail.com>
2026-03-20 20:58:34 +0200

    feat(header): improve UI of header buttons

--- src/shared/layout/Header/UserActions.tsx
+++ src/shared/layout/Header/UserActions.tsx
@@ -25,14 +25,14 @@ export function UserActions({
   return (
     <>
       <Button
-        variant="ghost"
+        variant="outline"
         size="icon"
         className="relative"
         onClick={onOpenCart}
       >
         <ShoppingCart />
         {count > 0 && (
-          <Badge className="absolute -right-1 -top-1 size-5 flex items-center justify-center text-xs">
+          <Badge className="absolute -right-2 -top-2 size-5 flex items-center justify-center text-xs">
             {count}
           </Badge>
         )}
@@ -40,7 +40,7 @@ export function UserActions({
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon">
+          <Button variant="outline" size="icon">
             <User className="h-5 w-5" />
           </Button>
         </DropdownMenuTrigger>
