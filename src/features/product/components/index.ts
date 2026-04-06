09100fb30c8f688a4949dd9b64e3932ebd372e56
Yuriy <liparg104@gmail.com>
2026-04-06 20:55:28 +0300

    feat: add Hot Deals section

--- src/features/product/components/index.ts
+++ src/features/product/components/index.ts
@@ -1,3 +1,4 @@
 export * from './Menu';
 export * from './Hero';
+export * from './HotDeals';
 export * from './ProductCard';
--- src/features/product/pages/HomePage.tsx
+++ src/features/product/pages/HomePage.tsx
@@ -1,10 +1,15 @@
-import { Hero, Menu } from '../components';
+import { Hero, HotDeals, Menu } from '../components';
 
 export function HomePage() {
   return (
-    <section className="section">
-      <Hero />
+    <>
+      <section className="section">
+        <Hero />
+      </section>
+      <section className="section border-t-0">
+        <HotDeals />
+      </section>
       {/* <Menu /> */}
-    </section>
+    </>
   );
 }
